2225daf5cc3c7cb08e7d8fc851f6bf245a831e5f
Sean McKenna <seanmck@DESKTOP-PFEISMH.localdomain>
2021-01-08 10:51:27 -0800

    Updating wsl zshrc and adding aliases

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -35,5 +35,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "Installing powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+echo "Installing kubectx"
+sudo apt install kubectx
+
 echo "Changing shell"
 zsh
